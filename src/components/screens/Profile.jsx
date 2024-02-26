b1eced19ec1fff0fd88964ca8638ea2435b01110
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2024-02-26 09:51:54 -0300

    seila

---
+++ src/components/screens/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native-web'
+
+function Profile() {
+  return (
+    <View>
+        <Text>Profile</Text>
+    </View>
+  )
+}
+
+export default Profile
